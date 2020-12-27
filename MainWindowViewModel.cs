704098ad0629fcdb9e355b573014f67498882da6
Jogger <beltchikov@gmx.de>
2020-12-27 21:05:52 +0100

    Configuration extensions

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace FmpAnalyzer
+{
+    public class MainWindowViewModel : DependencyObject
+    {
+        public static readonly DependencyProperty ConnectionStringProperty;
+
+        static MainWindowViewModel()
+        {
+            ConnectionStringProperty = DependencyProperty.Register("ConnectionString", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(String.Empty));
+        }
+
+        public MainWindowViewModel()
+        {
+            ConnectionString = Configuration.Instance["ConnectionString"];
+        }
+
+        public string ConnectionString
+        {
+            get { return (string)GetValue(ConnectionStringProperty); }
+            set { SetValue(ConnectionStringProperty, value); }
+        }
+
+
+
+    }
+}
