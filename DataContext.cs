401aca1b7345de7fc8ac0686c93c9b6e6bb2d0ca
Jogger <beltchikov@gmx.de>
2020-12-28 13:17:50 +0100

    EF added

---
+++ DataContext.cs
@@ -0,0 +1,47 @@
+﻿using FmpAnalyzer.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FmpAnalyzer
+{
+    /// <summary>
+    /// DataContext
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        private static DataContext _dataContext;
+        private static readonly object lockObject = new object();
+
+        public static DataContext Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (_dataContext == null)
+                    {
+                        _dataContext = new DataContext();
+                    }
+                    return _dataContext;
+                }
+            }
+        }
+
+        /// <summary>
+        /// OnConfiguring
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.Instance["ConnectionString"]);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<IncomeStatement> IncomeStatements { get; set; }
+        public DbSet<BalanceSheet> BalanceSheets { get; set; }
+        public DbSet<CashFlowStatement> CashFlowStatements { get; set; }
+    }
+}
--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 
@@ -8,6 +9,7 @@ namespace FmpAnalyzer
     public class MainWindowViewModel : DependencyObject
     {
         public static readonly DependencyProperty ConnectionStringProperty;
+        public RelayCommand CommandGo { get; set; }
 
         static MainWindowViewModel()
         {
@@ -17,6 +19,8 @@ namespace FmpAnalyzer
         public MainWindowViewModel()
         {
             ConnectionString = Configuration.Instance["ConnectionString"];
+
+            CommandGo = new RelayCommand(p => { OnCommandGo(p); });
         }
 
         public string ConnectionString
@@ -25,7 +29,11 @@ namespace FmpAnalyzer
             set { SetValue(ConnectionStringProperty, value); }
         }
 
+        private void OnCommandGo(object p)
+        {
+            //var topStocks = DataContext.Instance.Stocks.All(s => 1== 1).Take(10).Select(s => s.)
 
+        }
 
     }
 }
