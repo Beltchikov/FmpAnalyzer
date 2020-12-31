be8b7e378cba4ec35173a0927ac0d8edf0570f81
Jogger <beltchikov@gmx.de>
2020-12-31 16:58:56 +0100

    Intermediate

---
+++ Histogram.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FmpAnalyzer
+{
+    /// <summary>
+    /// Interaction logic for Histogram.xaml
+    /// </summary>
+    public partial class Histogram : UserControl
+    {
+        public static readonly DependencyProperty ItemsSourceProperty;
+
+        static Histogram()
+        {
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(Histogram), new PropertyMetadata(default(IEnumerable)));
+        }
+
+        public Histogram()
+        {
+            InitializeComponent();
+        }
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+
+
+    }
+}
