d63ff2ae951a11a7a29f3be9e2115cc383ca3bd9
Jogger <beltchikov@gmx.de>
2020-12-31 21:38:53 +0100

    Bar height conversion

--- Histogram.xaml.cs
+++ Histogram.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -39,17 +40,63 @@ namespace FmpAnalyzer
         }
     }
 
-    [ValueConversion(typeof(double), typeof(double))]
-    public class HistogramConverter : IValueConverter
+    /// <summary>
+    /// HistogramConverter
+    /// </summary>
+    public class HistogramConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return value;
+            if (!values.Any())
+            {
+                return values;
+            }
+
+            // Get history array and height of control
+            List<double> inputValuesList = new List<double>();
+            double height = 0;
+            foreach (var inputValue in values)
+            {
+                if (inputValue is List<double>)
+                {
+                    inputValuesList = (List<double>)inputValue;
+                }
+                if(inputValue is double)
+                {
+                    height = (double)inputValue;
+                }
+            }
+
+            // Are values there?
+            if(Double.IsNaN(height) || !inputValuesList.Any())
+            {
+                return inputValuesList;
+            }
+
+            // Convert ant return
+            return ConvertHistory(inputValuesList, height);
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            return value;
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable ConvertHistory(List<double> inputList, double height)
+        {
+            List<double> outputList = new List<double>();
+            
+            var max = inputList.Max();
+            var min = .0;
+            if (inputList.Where(v => v < 0).Any())
+            {
+                min = inputList.Min();
+            }
+            var range = max - min;
+            var koef = 0.8 * height / range;
+            inputList.ForEach(v => outputList.Add(v * koef));
+
+            return outputList;
         }
     }
 
