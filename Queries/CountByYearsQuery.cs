1c37eefdc7431dee192fcda6251bd6f0f3d2dfa5
Jogger <beltchikov@gmx.de>
2021-01-18 15:06:19 +0100

    FmpHelper.BuildDatesList

--- Queries/CountByYearsQuery.cs
+++ Queries/CountByYearsQuery.cs
@@ -22,7 +22,7 @@ namespace FmpAnalyzer.Queries
         /// <returns></returns>
         public int Run(int yearFrom, int yearTo, List<string> dates)
         {
-            var datesList = BuildDatesList(yearFrom, yearTo, dates);
+            var datesList = FmpHelper.BuildDatesList(yearFrom, yearTo, dates);
 
             int result = (from income in DataContext.IncomeStatements
                           where datesList.Contains(income.Date)
@@ -30,26 +30,5 @@ namespace FmpAnalyzer.Queries
             return result;
         }
 
-        /// <summary>
-        /// BuildDatesList
-        /// </summary>
-        /// <param name="yearFrom"></param>
-        /// <param name="yearTo"></param>
-        /// <param name="dates"></param>
-        /// <returns></returns>
-        private List<string> BuildDatesList(int yearFrom, int yearTo, List<string> dates)
-        {
-            List<string> resultList = new List<string>();
-
-            for (int year = yearFrom; year <= yearTo; year++)
-            {
-                foreach (var date in dates)
-                {
-                    resultList.Add(year.ToString() + date[4..]);
-                }
-            }
-
-            return resultList;
-        }
     }
 }
