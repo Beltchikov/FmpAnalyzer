05736df6e91740fdc7cadae5a051feaf4f9ab915
Jogger <beltchikov@gmx.de>
2021-01-26 04:27:22 +0100

    Improvements

--- Queries/CompounderCountQueryParams.cs
+++ Queries/CompounderCountQueryParams.cs
@@ -10,6 +10,8 @@ namespace FmpAnalyzer.Queries
         public int YearTo { get; internal set; }
         public double RoeFrom { get; set; }
         public double ReinvestmentRateFrom { get; set; }
+        public double RoeTo { get; internal set; }
+        public double ReinvestmentRateTo { get; internal set; }
         public int RoeGrowthKoef { get; internal set; }
     }
 }
